cd7b3ccfeb92035bfa2c3dab59d64af597ad2305
Nathan Coder <nathancoder1@Nathans-MacBook-Pro.local>
2018-01-19 23:04:42 -0500

    to create repo

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ var apiRoutes = require("./app/routing/apiRoutes.js");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
